ce950f4bb6a6f993fac7b3b19e35331dc9501654
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-03-22 17:48:52 +0900

    :construction: Complete chapter5-7

--- part4-chapter5-7/app/src/main/java/kr/co/fastcampus/part4/part5_7/service/GithubService.kt
+++ part4-chapter5-7/app/src/main/java/kr/co/fastcampus/part4/part5_7/service/GithubService.kt
@@ -3,8 +3,13 @@ package kr.co.fastcampus.part4.part5_7.service
 import kr.co.fastcampus.part4.part5_7.model.Repo
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GithubService {
     @GET("users/{user}/repos")
-    suspend fun listRepos(@Path("user") user: String): List<Repo>
+    suspend fun listRepos(
+        @Path("user") user: String,
+        @Query("sort") sort: String = "updated",
+        @Query("direction") direction: String = "desc"
+    ): List<Repo>
 }
